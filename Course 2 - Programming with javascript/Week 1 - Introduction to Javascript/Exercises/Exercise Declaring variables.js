995bc1698ad1a4dfb87cfa4537f4a0f048a23198
Yasser <yvsser1@gmail.com>
2023-06-01 16:30:42 +0100

    added variables exercise

---
+++ Course 2 - Programming with javascript/Week 1 - Introduction to Javascript/Exercises/Exercise Declaring variables.js
@@ -0,0 +1,58 @@
+//Task 1 solution to: Declare a new variable named petDog and give it the name Rex.
+
+var petDog = 'Rex';
+
+//Task 2 solution to: Declare a new variable named petCat and give it the name Pepper.
+
+var petCat = 'Pepper';
+
+//Task 3 solution to: Console log the petDog variable.
+
+var petDog = 'Rex'; 
+console.log(petDog);
+
+//Task 4 solution to: Console log the petCat variable.
+
+var petCat = 'Pepper';
+console.log(petCat);
+
+//Task 5 solution to: Console.log the string "My pet dog's name is: " and the petDog variable.
+
+var petDog = 'Rex'; 
+console.log("My pet dog's name is: " + petDog);
+
+//Task 6 solution to: Console.log the string "My pet cat's name is: " and the petCat variable.
+
+var petCat = 'Pepper';
+console.log("My pet cat's name is: " + petCat);
+
+//Task 7 solution to: Declare another variable and name it catSound. Assign the string of "purr" to it.
+
+var catSound = "purr";
+
+//Task 8 solution to: Declare another variable and name it dogSound. Assign the string of "woof" to it.
+
+var dogSound = "woof";
+
+//Task 9 solution to: Console.log the variable petDog, then the string "says", then the variable dogSound.
+
+var petDog = 'Rex'; 
+var dogSound = "woof"; 
+console.log(petDog, "says", dogSound);
+
+//Task 10 solution to: Console.log the variable petCat, then the string "says", then the variable catSound.
+
+var petCat = 'Pepper'; 
+var catSound = "purr"; 
+console.log(petCat, "says", catSound);
+
+//Task 11 solution to: Reassign the value stored in catSound to the string "meow".
+
+var catSound = "purr"; 
+catSound = "meow";
+
+//Task 12 solution to: Console.log the variable petCat, then the string "now says", then the variable catSound.
+
+var petCat = 'Pepper'; 
+var catSound = "meow"; 
+console.log(petCat, "now says", catSound);
