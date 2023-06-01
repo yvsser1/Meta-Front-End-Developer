7e6959b549ffdb8cc5c6f779643956c093e51a0f
Yasser <yvsser1@gmail.com>
2023-06-01 23:49:08 +0100

    week 1 progress

---
+++ Course 2 - Programming with javascript/Week 1 - Introduction to Javascript/Exercises/Exercise Repetitive tasks with loops.js
@@ -0,0 +1,40 @@
+// Task 1
+for (var i=1; i<=5; i++){
+console.log(i);
+}
+console.log('Counting completed!');
+
+// Task 2
+
+for(var i = 5; i>=1; i--){
+    console.log(i);
+}
+console.log('Countdown finished!');
+// Task 3
+
+var i = 1;
+
+while(i<=5){
+    console.log(i);
+    i++;
+}
+console.log('Counting completed!');
+
+// Task 4
+
+var i = 5;
+
+while(i>=1){
+    console.log(i);
+    i--;
+}
+console.log('Countdown finished!');
+
+// Task 5
+
+var i = 2018;
+
+while( i<=2022 ){
+    console.log(i);
+    i++;
+}
